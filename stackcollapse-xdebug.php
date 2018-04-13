4bc6355db2d89d00a3940814513d10d3f69e42e7
Miriam Lauter <lauter.miriam@gmail.com>
2018-04-13 12:56:44 -0400

    Add GPL license

--- stackcollapse-xdebug.php
+++ stackcollapse-xdebug.php
@@ -1,4 +1,25 @@
 #!/usr/bin/php
+#
+# Copyright 2018 Miriam Lauter (lauter.miriam@gmail.com).  All rights reserved.
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+#  (http://www.gnu.org/copyleft/gpl.html)
+#
+# 13-Apr-2018   Miriam Lauter   Created this.
+
 <?php
 ini_set('error_log', null);
 $optind = null;
