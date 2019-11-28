1c11ad521e6c3bab4883718164879febdef5c2b7
UnpluggedCoder <unpluggedcoder@outlook.com>
2019-11-28 14:12:25 +0800

    Merge vtune memory scripts

--- stackcollapse-vtune-mc.pl
+++ stackcollapse-vtune-mc.pl
@@ -4,7 +4,7 @@
 #
 # Parses the CSV file containing a call tree from Intel VTune memory-consumption profiler and produces an output suitable for flamegraph.pl.
 #
-# USAGE: perl stackcollapse-vtune-mc.pl infile > outfile
+# USAGE: perl stackcollapse-vtune-mc.pl [options] infile > outfile
 #
 # WORKFLOW:
 #
@@ -16,32 +16,53 @@
 #
 # 2. Export raw VTune data to csv file:
 #    ### for Intel VTune 2019
-#    amplxe-cl -R top-down -call-stack-mode all -column="Allocations:Self","Module" -report-out allocations.csv -format csv -csv-delimiter comma -r mc_tachyon
+#    amplxe-cl -R top-down -call-stack-mode all \
+#			-column="Allocations:Self","Allocation Size:Self","Module" \
+#			-report-out allocations.csv -format csv \
+#			-csv-delimiter comma -r mc_tachyon
 #
 # 3. Generate a flamegraph:
-#
+#    ## Generate for allocations amount.
 #    perl stackcollapse-vtune-mc.pl allocations.csv > out.folded
 #    perl flamegraph.pl --countname=allocations out.folded > vtune_tachyon_mc.svg
+#
+#    ## Or you can generate for allocation size in bytes.
+#    perl stackcollapse-vtune-mc.pl -s allocations.csv > out.folded
+#    perl flamegraph.pl --countname=allocations out.folded > vtune_tachyon_mc_size.svg
 #
 # AUTHOR: Rohith Bakkannagari
 # 27-Nov-2019	UnpluggedCoder		Forked from stackcollapse-vtune.pl, for memory-consumption flamegraph
 
 use strict;
+use Getopt::Long;
+
+sub usage {
+	die <<USAGE_END;
+Usage : $0 [options] allocations.csv > out.folded\n
+	--size		# Accumulate allocation size in bytes instead of allocation counts.\n
+NOTE : The csv file should exported by `amplxe-cl` tool with the exact -column parameter shows below.
+	amplxe-cl -R top-down -call-stack-mode all \
+		-column="Allocations:Self","Allocation Size:Self","Module" \
+		-report-out allocations.csv -format csv \
+		-csv-delimiter comma -r mc_tachyon
+USAGE_END
+}
 
 # data initialization
 my @stack = ();
 my $rowCounter = 0; # flag for row number
 
+my $accSize = '';
+GetOptions ('size' => \$accSize)
+or usage();
+
 my $numArgs = $#ARGV + 1;
-if ($numArgs != 1)
-{
-print "$ARGV[0]\n";
-print "Usage : stackcollapse-vtune-mc.pl <out.cvs> > out.txt\n";
-exit;
+if ($numArgs != 1){
+	usage();
+	exit;
 }
 
 my $inputCSVFile = $ARGV[0];
-
 open(my $fh, '<', $inputCSVFile) or die "Can't read file '$inputCSVFile' [$!]\n";
 
 while (my $currLine = <$fh>){
@@ -55,20 +76,28 @@ while (my $currLine = <$fh>){
 	$currLine =~ s/\"//g;
 
 	### for Intel VTune 2019
-	### Function Stack,Allocations:Self,Module
-	$currLine =~ /(\s*)(.*?),([0-9]+?),(.*)/ or die "Error in regular expression on the current line $currLine\n";
-	my $func = $2.'('.$4.')';
+	### CSV header should be like below
+	### Function Stack,Allocation Size:Self,Deallocation Size:Self,Allocations:Self,Module
+	$currLine =~ /(\s*)(.*?),([0-9]*?\.?[0-9]*?),([0-9]*?\.?[0-9]*?),([0-9]+?),(.*)/ or die "Error in regular expression on the current line $currLine\n";
+	my $func = $2.'('.$6.')';	# function(module)
 	my $depth = length ($1);
-	my $allocs = $3; # allocations
+	my $allocBytes = $3; 	# allocation size
+	my $allocs = $5; 		# allocations
 
 	my $tempString = '';
 	$stack [$depth] = $func;
-	next if $allocs == 0;
-
-	foreach my $i (0 .. $depth - 1) {
-		$tempString = $tempString.$stack[$i].";";
+	if ($accSize){
+		next if $allocBytes eq '';
+		foreach my $i (0 .. $depth - 1) {
+			$tempString = $tempString.$stack[$i].";";
+		}
+		$tempString = $tempString.$func." $allocBytes\n";
+	} else {
+		next if $allocs == 0;
+		foreach my $i (0 .. $depth - 1) {
+			$tempString = $tempString.$stack[$i].";";
+		}
+		$tempString = $tempString.$func." $allocs\n";
 	}
-	$tempString = $tempString.$func." $allocs\n";
-
 	print "$tempString";
 }
